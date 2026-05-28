f9c466d184905c26f677db36fd71113b31ec3484
LorenzoMinon <lorenzominon01@gmail.com>
2026-05-28 15:09:19 +0000

    add InsertCoinRecord to db package

--- db/db.go
+++ db/db.go
@@ -2,8 +2,10 @@ package db
 
 import (
 	"context"
+	"fmt"
 	"os"
 
+	"github.com/LorenzoMinon/coinflow/pipeline"
 	"github.com/jackc/pgx/v5"
 )
 
@@ -13,4 +15,12 @@ func Connect() (*pgx.Conn, error) {
 	conn, err := pgx.Connect(context.Background(), db_url)
 
 	return conn, err
+}
+
+func InsertCoinRecord(conn *pgx.Conn, record pipeline.CoinRecord) error {
+	_, err := conn.Exec(context.Background(), "INSERT INTO coin_prices (coin_id, symbol, price_usd, change_24h, sentiment) VALUES ($1, $2, $3, $4, $5)", record.CoinID, record.Symbol, record.PriceUSD, record.Change24h, record.Sentiment)
+	if err != nil {
+		fmt.Println("Error while inserting coin data!", err)
+	}
+	return err
 }
