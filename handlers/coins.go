eb9c6f4274f62aa281aafe2496921828444e4e1a
LorenzoMinon <lorenzominon01@gmail.com>
2026-05-28 18:40:15 +0000

    add GetCoins and GetLatest handlrs

--- handlers/coins.go
+++ handlers/coins.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
 
@@ -16,6 +17,15 @@ type PipelineResult struct {
 
 type Handler struct {
 	DB *pgx.Conn
+}
+type CoinResponse struct {
+	ID        int     `json:"id"`
+	CoinID    string  `json:"coin_id"`
+	Symbol    string  `json:"symbol"`
+	PriceUSD  float64 `json:"price_usd"`
+	Change24h float64 `json:"change_24h"`
+	Sentiment string  `json:"sentiment"`
+	FetchedAt string  `json:"fetched_at"`
 }
 
 func (h *Handler) RunPipeline(w http.ResponseWriter, r *http.Request) {
@@ -46,4 +56,56 @@ func (h *Handler) RunPipeline(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(data)
+}
+
+func (h *Handler) GetCoins(w http.ResponseWriter, r *http.Request) {
+	my_query := "SELECT * FROM coin_prices ORDER BY fetched_at DESC"
+	rows, err := h.DB.Query(context.Background(), my_query)
+	if err != nil {
+		http.Error(w, "error querying", http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var coins []CoinResponse
+
+	for rows.Next() {
+		var c CoinResponse
+		rows.Scan(&c.ID, &c.CoinID, &c.Symbol, &c.PriceUSD, &c.Change24h, &c.Sentiment, &c.FetchedAt)
+		coins = append(coins, c)
+	}
+	data, err := json.Marshal(coins)
+	if err != nil {
+		http.Error(w, "error parsing to json", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
+
+}
+
+func (h *Handler) GetLatest(w http.ResponseWriter, r *http.Request) {
+	my_query := "SELECT DISTINCT ON (coin_id) coin_id, symbol, price_usd, change_24h, sentiment, fetched_at FROM coin_prices ORDER BY coin_id, fetched_at DESC"
+	rows, err := h.DB.Query(context.Background(), my_query)
+	if err != nil {
+		http.Error(w, "error querying", http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var coins []CoinResponse
+
+	for rows.Next() {
+		var c CoinResponse
+		rows.Scan(&c.CoinID, &c.Symbol, &c.PriceUSD, &c.Change24h, &c.Sentiment, &c.FetchedAt)
+		coins = append(coins, c)
+	}
+	data, err := json.Marshal(coins)
+	if err != nil {
+		http.Error(w, "error parsing to json", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
+
 }
